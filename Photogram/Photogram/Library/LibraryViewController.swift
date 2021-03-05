96886dde1840776b0a179a7f2ff0b7db72cd0980
hoanghy <huyhoanghy98@gmail.com>
2021-03-05 14:02:29 +0700

    add tabbar in Home.storyboard

---
+++ Photogram/Photogram/Library/LibraryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LibraryViewController.swift
+//  Photogram
+//
+//  Created by LTT on 05/03/2021.
+//
+
+import Foundation
+import UIKit
+
+class LibraryViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Photogram/Photogram/Search/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  Photogram
+//
+//  Created by LTT on 05/03/2021.
+//
+
+import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
