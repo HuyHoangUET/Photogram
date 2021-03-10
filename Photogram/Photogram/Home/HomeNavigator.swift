5196ed48b00d31f7b9db973becf85819cff492af
hoanghy <huyhoanghy98@gmail.com>
2021-03-10 09:10:03 +0700

    Update HomeNavigator.swift

--- Photogram/Photogram/Home/HomeNavigator.swift
+++ Photogram/Photogram/Home/HomeNavigator.swift
@@ -23,6 +23,8 @@ class DefaultHomeNavigator: HomeNavigator {
     func logout() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let loginView = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+        let navigator = DefaultLoginNavigator(navigationController: self.navigationController)
+        loginView.viewModel = LoginViewModel(navigator: navigator)
         navigationController.setViewControllers([loginView], animated: true)
     }
     
