f7a083d577828e6e63b8129f6d9a43fb6005982e
hoanghy <huyhoanghy98@gmail.com>
2021-03-16 10:16:55 +0700

    create Responsitory

---
+++ Photogram/Photogram/Base/Protocol/AbstractResponsitory.swift
@@ -0,0 +1,11 @@
+//
+//  AbstractResponsitory.swift
+//  Photogram
+//
+//  Created by LTT on 16/03/2021.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
---
+++ Photogram/Photogram/Platform/Responsitory/Respository.swift
@@ -0,0 +1,62 @@
+//
+//  Respository.swift
+//  Photogram
+//
+//  Created by LTT on 16/03/2021.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import Firebase
+
+protocol AbstractResponsitory {
+    associatedtype T
+    func signIn(account: T) -> Observable<NSError>
+    func signUp(account: T) -> Observable<NSError>
+    func signOut() -> Observable<Void>
+}
+
+final class Responsitory<T: Account>: AbstractResponsitory {
+    func signIn(account: T) -> Observable<NSError> {
+        return Observable.create{ observer in
+            Auth.auth().signIn(withEmail: account.username, password: account.password) {_, error in
+                if let error = error as NSError? {
+                    observer.onNext(error)
+                    observer.onCompleted()
+                } else {
+                    observer.onNext(NSError())
+                    observer.onCompleted()
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func signUp(account: T) -> Observable<NSError> {
+        return Observable.create{ observer in
+            Auth.auth().createUser(withEmail: account.username, password: account.password) { _, error in
+                if let error = error as NSError? {
+                    observer.onNext(error)
+                    observer.onCompleted()
+                } else {
+                    observer.onNext(NSError())
+                    observer.onCompleted()
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func signOut() -> Observable<Void> {
+        return Observable.create{ observer in
+            do {
+                try Auth.auth().signOut()
+            } catch {
+                print("error")
+            }
+            return Disposables.create()
+        }
+    }
+    
+}
