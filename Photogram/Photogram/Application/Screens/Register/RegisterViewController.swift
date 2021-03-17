6c1b499b0ab585d853e6bf69d424700e89262d36
hoanghy <huyhoanghy98@gmail.com>
2021-03-17 09:35:14 +0700

    update RegisterViewController

--- Photogram/Photogram/Application/Screens/Register/RegisterViewController.swift
+++ Photogram/Photogram/Application/Screens/Register/RegisterViewController.swift
@@ -29,32 +29,6 @@ class RegisterViewController: UIViewController {
         bindViewModel()
     }
     
-    // MARK: action
-    @IBAction func register(_ sender: Any) {
-//        if passwordText.text == confirmPasswordText.text {
-//            viewModel.requestRegister(email: emailText.text ?? "", password: passwordText.text ?? "", completetion: {error in
-//                if error == NSError() {
-//                    self.navigationController?.popToRootViewController(animated: true)
-//                } else {
-//                    switch AuthErrorCode(rawValue: error.code) {
-//                    case .missingEmail:
-//                        self.errorOfEmailLabel.text = error.localizedDescription
-//                    case .emailAlreadyInUse:
-//                        self.errorOfEmailLabel.text = error.localizedDescription
-//                    case .invalidEmail:
-//                        self.errorOfEmailLabel.text = error.localizedDescription
-//                    case .weakPassword:
-//                        self.errorOfPasswordLabel.text = error.localizedDescription
-//                    default:
-//                        print("Error: \(error.localizedDescription)")
-//                    }
-//                }
-//            })
-//        } else {
-//            errorOfConfirmPasswordLabel.text = "Password confirmation doesn't match Password."
-//        }
-    }
-    
     func bindViewModel() {
         let input = RegisterViewModel.Input(username: emailTextField.rx.text.orEmpty.asDriver(),
                                             password: passwordTextField.rx.text.orEmpty.asDriver(),
