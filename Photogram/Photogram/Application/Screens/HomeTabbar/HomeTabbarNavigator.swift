62f26c9503bed6fa8deb201e6650542c0b6750c5
hoanghy <huyhoanghy98@gmail.com>
2021-03-23 08:52:17 +0700

    Update HomeTabbarNavigator.swift

--- Photogram/Photogram/Application/Screens/HomeTabbar/HomeTabbarNavigator.swift
+++ Photogram/Photogram/Application/Screens/HomeTabbar/HomeTabbarNavigator.swift
@@ -9,7 +9,6 @@ import Foundation
 import UIKit
 
 protocol HomeTabbarNavigator {
-    func logOut()
 }
 
 class DefaultHomeTabbarNavigator: HomeTabbarNavigator {
@@ -17,15 +16,5 @@ class DefaultHomeTabbarNavigator: HomeTabbarNavigator {
     
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
-    }
-    
-    func logOut() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        guard let loginView = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController else {return}
-        let navigator = DefaultLoginNavigator(navigationController: self.navigationController)
-        let respository = Respository()
-        let useCase = LoginUseCase(respository: respository)
-        loginView.viewModel = LoginViewModel(navigator: navigator, useCase: useCase)
-        navigationController.setViewControllers([loginView], animated: true)
     }
 }
