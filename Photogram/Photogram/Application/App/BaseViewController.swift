011b13bb94931cb99975185cd380ef4e4a41b302
hoanghy <huyhoanghy98@gmail.com>
2021-03-23 14:05:12 +0700

    create BaseViewModel

---
+++ Photogram/Photogram/Application/App/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  Photogram
+//
+//  Created by LTT on 23/03/2021.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class BaseViewController: UIViewController {
+    private let bag = DisposeBag()
+    func bindViewmodel() {
+    }
+}
